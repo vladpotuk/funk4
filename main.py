0b490ca3de13a043746471f839868ab02b794277
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-10-03 23:25:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def find_minimum(a, b, c, d, e):
+ minimum = min(a, b, c, d, e)
+ return minimum
+num1 = 8
+num2 = 4
+num3 = 12
+num4 = 5
+num5 = 9
+
+result = find_minimum(num1, num2, num3, num4, num5)
+print(f"Мінімальне число серед {num1}, {num2},{num3},{num4},{num5} дорівнює: {result}")
+
